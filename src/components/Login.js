f4de8aa579d383b0eb0916154be68b62372c8242
Kayla Dailey <kayladailey000@gmail.com>
2020-01-14 16:50:52 -0600

    3rd  commit Create Forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 
-const Login = () =>  {
-  return (
-    <div className="Login">
-        <h1>Login</h1>
-    </div>
-  );
-}
-
-export default Login;
+class Login extends React.Component {
+    state = {
+      credentials: {
+        username: '',
+        password: ''
+      }
+    };
+  
+    handleChange = e => {
+      this.setState({
+        credentials: {
+          ...this.state.credentials,
+          [e.target.name]: e.target.value
+        }
+      });
+    };
+  
+    login = e => {
+      e.preventDefault();
+      // axiosWithAuth ==> ?? an axios instance; .post() ==> ?? promise
+      axiosWithAuth()
+        .post('auth/login', this.state.credentials)
+        .then(res => {
+          localStorage.setItem('token', res.data.payload);
+          // redirect to the apps main page?
+          this.props.history.push('/products');
+        })
+        .catch(err => console.log(err));
+    };
+  
+    render() {
+      return (
+           <div>
+          <form onSubmit={this.login}>
+            <h1>Login</h1>
+            <input
+              type="text"
+              name="username"
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            />
+            <button>Log in</button>
+          </form>
+        </div>
+      );
+    }
+  }
+  
+  export default Login;
+  
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,12 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+const Signup = props => {
+    const [user, setUser] = useState({
+      fullname: '',
+      city:'',
+      password: '',
+      username: ''
+    });
 
-const Signup = () =>  {
-  return (
-    <div className="Signup">
-    <h1>Sign up</h1>
-    </div>
-  );
-}
-
+  
+    const handleChange = e => {
+      setUser({
+        ...user,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+  
+         axios()
+            .post('auth/register', user)
+            .then(res => {
+            console.log('Add User:', res);
+            setUser({
+                fullname: '',
+                city:'',
+                password: '',
+                username: ''
+            });
+          props.history.push('/login');
+          window.location.reload(false);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
+            <div className='signup'>
+                <input
+                  type='text'
+                  placeholder='Full Name'
+                  name='fullName'
+                  value={user.fullName}
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  placeholder='City'
+                  name='city'
+                  type='text'
+                  value={user.city}
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  placeholder='Password'
+                  name='password'
+                  type='password'
+                  value={user.password}
+                  onChange={handleChange}
+                  required
+                />
+                
+                <input
+                  placeholder='Username'
+                  name='username'
+                  type='text'
+                  value={user.username}
+                  onChange={handleChange}
+                  required
+                />
+              <div>
+                <button type='submit'>Sign Up</button>
+              </div>
+                 <Link to='/UserLogin'>I already have an account</Link>
+               
+         </div>
+        </form>
+      </div>
+    );
+  };
 export default Signup;
